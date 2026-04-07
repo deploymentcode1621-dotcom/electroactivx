9fadb2a1b939f33adf952b8f55cd900233643cfc
AdityaRaut <adityaraut6029@gmail.com>
2026-04-07 22:36:03 +0530

    final changes

--- elektroactivx/src/App.jsx
+++ elektroactivx/src/App.jsx
@@ -1,19 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Products from "./pages/Products";
+import Blogs from "./pages/Blogs";
+import Career from "./pages/Career";
+import Contact_us from "./pages/Contact_us";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Application from "./pages/Application"
+import MainLayout from "./layouts/MainLayout";
+import PolymerDispersion from "./pages/PolymerDispersion"
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+
+        {/* ✅ Routes WITH Header + Footer */}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/applications" element={<Application/>} />
+          <Route path="/polymerdispersion" element={<PolymerDispersion/>} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/career" element={<Career />} />
+          <Route path="/contact_us" element={<Contact_us />} />
+        </Route>
+
+  
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
