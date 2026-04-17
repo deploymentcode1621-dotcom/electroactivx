401ef6067ef2809f4fd752f04e79a1cd067b5973
AdityaRaut <deploymentcode1621@gmail.com>
2026-04-17 23:22:08 +0530

    scrolling issue

--- elektroactivx/src/App.jsx
+++ elektroactivx/src/App.jsx
@@ -16,41 +16,34 @@ import AboutUs from "./pages/AboutUs";
 import Application from "./pages/Application";
 import MainLayout from "./layouts/MainLayout";
 import PolymerDispersion from "./pages/PolymerDispersion";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />   {/* 👈 add here */}
+
       <Routes>
-
         <Route element={<MainLayout />}>
-
           <Route path="/" element={<Home />} />
-
           <Route path="/about" element={<AboutUs />} />
-
           <Route path="/products" element={<Products />} />
 
-          {/* Products */}
           <Route path="/products/dispersion" element={<Dispersion />} />
-<Route path="/products/pani-eb" element={<PaniEB />} />
-<Route path="/products/pani-kot" element={<PaniKot />} />
-<Route path="/products/masterbatch" element={<Masterbatch />} />
-<Route path="/products/ormecon-st" element={<OrmeconST />} />
-<Route path="/products/ormecon-al" element={<OrmeconAL />} />
+          <Route path="/products/pani-eb" element={<PaniEB />} />
+          <Route path="/products/pani-kot" element={<PaniKot />} />
+          <Route path="/products/masterbatch" element={<Masterbatch />} />
+          <Route path="/products/ormecon-st" element={<OrmeconST />} />
+          <Route path="/products/ormecon-al" element={<OrmeconAL />} />
 
           <Route path="/applications" element={<Application />} />
-
           <Route path="/polymerdispersion" element={<PolymerDispersion />} />
-
           <Route path="/blogs" element={<Blogs />} />
-
           <Route path="/contact_us" element={<Contact_us />} />
-
         </Route>
-
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App; 
+export default App;
